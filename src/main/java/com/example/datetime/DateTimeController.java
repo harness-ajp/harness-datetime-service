3f283bbc9f1e6ad454eb50df73407746d60fef07
harness-ajp <anthony.pahl@harness.io>
2026-07-16 20:36:44 -0400

    commented out FME temporarily

--- src/main/java/com/example/datetime/DateTimeController.java
+++ src/main/java/com/example/datetime/DateTimeController.java
@@ -21,44 +21,44 @@ import java.util.concurrent.TimeoutException;
 
 @RestController
 public class DateTimeController {
+//
+//    private static SplitClientConfig config;
+//    static SplitFactory splitFactory;
+//    static SplitClient client;
 
-    private static SplitClientConfig config;
-    static SplitFactory splitFactory;
-    static SplitClient client;
+//    @Value("${harness.fme.key}")
+//    private String HARNESS_FME_KEY;
 
-    @Value("${harness.fme.key}")
-    private String HARNESS_FME_KEY;
-
-    @PostConstruct
-    public void init() throws IOException, URISyntaxException {
-        config = io.split.client.SplitClientConfig.builder()
-                .setBlockUntilReadyTimeout(10000)
-                .build();
-        splitFactory = SplitFactoryBuilder.build(HARNESS_FME_KEY, config);
-        client = splitFactory.client();
-    }
+//    @PostConstruct
+//    public void init() throws IOException, URISyntaxException {
+//        config = io.split.client.SplitClientConfig.builder()
+//                .setBlockUntilReadyTimeout(10000)
+//                .build();
+//        splitFactory = SplitFactoryBuilder.build(HARNESS_FME_KEY, config);
+//        client = splitFactory.client();
+//    }
 
     @GetMapping("/api/now")
     public String getCurrentDateTime() throws Exception {
 
-        try {
-            client.blockUntilReady();
-        } catch (TimeoutException | InterruptedException e) {
+//        try {
+//            client.blockUntilReady();
+//        } catch (TimeoutException | InterruptedException e) {
             // log & handle
-        }
+//        }
 
         // Search for a specific treatment
-        String treatment = client.getTreatment("key", "dark_side");
+//        String treatment = client.getTreatment("key", "dark_side");
 
         Map<String, String> response = new HashMap<>();
         response.put("datetime", ZonedDateTime.now().toString());
 
-        if (treatment.contains("on")) {
+//        if (treatment.contains("on")) {
             ZonedDateTime original = ZonedDateTime.now(); // Current time in system timezone
             ZonedDateTime newTime = original.withZoneSameInstant(ZoneId.of("Pacific/Fiji")); // +12 from UTC
 
             response.put("darkSide", newTime.toString());
-        }
+//        }
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
